2a5859b4c0f58f733b383a18de760b9049dd53d5
Eunsoo Shin <me@esinx.net>
2022-09-23 05:40:24 +0900

    zsh setup

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt update
+sudo apt install zsh
+
+# install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
